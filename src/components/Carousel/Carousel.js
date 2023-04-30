6547e5e4d0938752240588449b2c4fbc047538cf
iarshtejay <arshtejay@gmail.com>
2023-04-30 12:01:18 -0300

    feat: basic carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./Carousel.css";
+
+export const Carousel = () => {
+  const [artworkOpen, setArtworkOpen] = useState({
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+  });
+
+  const toggleArtworkExpand = (key) => {
+    setArtworkOpen((prevState) => {
+      return { ...prevState, [key]: !prevState[key] };
+    });
+  };
+  
+  const artworks = [
+    { imageURL: "https://source.unsplash.com/gYl-UtwNg_I/1500x1500" },
+    { imageURL: "https://source.unsplash.com/rFKUFzjPYiQ/1500x1500" },
+    {
+      imageURL:
+        "https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d)",
+    },
+    { imageURL: "https://source.unsplash.com/ITjiVXcwVng/1500x1500" },
+    { imageURL: "https://source.unsplash.com/3MNzGlQM7qs/1500x1500" },
+  ];
+
+  return (
+    <div className="carousel">
+      {artworks.map((artwork, ind) => (
+        <div
+          className={`artwork ${artworkOpen[ind] ? "open" : ""}`}
+          style={{ background: `url(${artwork.imageURL})` }}
+          onClick={(event) => toggleArtworkExpand(ind)}
+          key={ind}
+        />
+      ))}
+    </div>
+  );
+};
