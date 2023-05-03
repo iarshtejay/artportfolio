3e2fea7d7d95dce6915456e4000d6d379ffe88ba
iarshtejay <arshtejay@gmail.com>
2023-05-03 12:14:06 -0300

    feat: add icons to category grid

--- src/components/CategoryGrid/CategoryGrid.js
+++ src/components/CategoryGrid/CategoryGrid.js
@@ -2,19 +2,27 @@ import React from "react";
 import "./CategoryGrid.css";
 import categories from "../../data/categories.json";
 import { Link } from "react-router-dom";
+import icons from "../../icons/icons";
 
 const CategoryGrid = () => {
   return (
-    <div className="CategoryGrid-container">
-      {categories.map((category) => {
-        return (
-          <div className="CategoryGrid-item">
-            <Link to={`/${category.toLocaleLowerCase()}`}>
-              <p className="CategoryGrid-link">{category}</p>
-            </Link>
-          </div>
-        );
-      })}
+    <div>
+      <p className="CategoryGrid-header">Categories</p>
+      <div className="CategoryGrid-container">
+        {categories.map((category) => {
+          return (
+            <div className="CategoryGrid-item">
+              <Link to={`/${category.toLocaleLowerCase()}`}>
+                <img
+                  src={icons[category.toLowerCase().replace("-", "")]}
+                  className="CategoryGrid-icon"
+                />
+                <p className="CategoryGrid-link">{category}</p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/icons/icons.js
@@ -0,0 +1,8 @@
+import acrylic from "./acrylic.png";
+import charcoal from "./charcoal.png";
+import mixmedia from "./mix-media.png";
+import pencil from "./pencil.png";
+import watercolor from "./watercolor.png";
+
+const icons = { acrylic, charcoal, mixmedia, pencil, watercolor };
+export default icons;
