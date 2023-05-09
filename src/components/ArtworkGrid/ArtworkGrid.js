452391281cc199354cbcd7d304edc8b363bad17b
iarshtejay <arshtejay@gmail.com>
2023-05-09 12:11:26 -0300

    feat: add spinner

--- src/components/ArtworkGrid/ArtworkGrid.js
+++ src/components/ArtworkGrid/ArtworkGrid.js
@@ -3,34 +3,38 @@ import "./ArtworkGrid.css";
 import { useContext } from "react";
 import { ArtworksContext } from "../../contexts/ArtworksContext";
 import { Link, useParams } from "react-router-dom";
+import BarLoader from "react-spinners/BarLoader";
 
 const ArtworkGrid = () => {
   const params = useParams();
   const category = params.categoryName;
   const { artworks, artworksLoading } = useContext(ArtworksContext);
 
-  return artworksLoading ? (
-    <div className="ArtworkGrid-spinner-container">
-      <div className="ArtworkGrid-spinner" />
-    </div>
-  ) : (
-    <div className="ArtworkGrid-container">
-      {artworks
-        .filter((artwork_) =>
-          category.includes(artwork_.category.toLocaleLowerCase())
-        )
-        .map((artwork) => {
-          return (
-            <Link to={`/artwork/${artwork.id}`}>
-              <div className="ArtworkGrid-item">
-                <img src={artwork.thumbnailURL} width={"200em"} />
-                <p>{artwork.name}</p>
-                {/* TODO: Remove category later on*/}
-                <p>{artwork.category}</p>
-              </div>
-            </Link>
-          );
-        })}
+  return (
+    <div>
+      <div className={artworksLoading ? "spinner-container" : ""}>
+        <BarLoader loading={artworksLoading} />
+      </div>
+      {!artworksLoading && (
+        <div className="ArtworkGrid-container">
+          {artworks
+            .filter((artwork_) =>
+              category.includes(artwork_.category.toLocaleLowerCase())
+            )
+            .map((artwork) => {
+              return (
+                <Link to={`/artwork/${artwork.id}`}>
+                  <div className="ArtworkGrid-item">
+                    <img src={artwork.thumbnailURL} width={"200em"} />
+                    <p>{artwork.name}</p>
+                    {/* TODO: Remove category later on*/}
+                    <p>{artwork.category}</p>
+                  </div>
+                </Link>
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -2,12 +2,13 @@ import React, { useState, useContext } from "react";
 import { ArtworksContext } from "../../contexts/ArtworksContext";
 import "./Carousel.css";
 import icons from "../../icons/icons";
+import BarLoader from "react-spinners/BarLoader";
 
 export const Carousel = () => {
   const featuredCount = 5;
   const [openArtwork, setOpenArtwork] = useState(2);
 
-  const { artworks } = useContext(ArtworksContext);
+  const { artworks, artworksLoading } = useContext(ArtworksContext);
 
   const changeArtworkOnDisplay = (key, delta) => {
     setOpenArtwork((featuredCount + key + delta) % featuredCount);
@@ -15,37 +16,44 @@ export const Carousel = () => {
 
   return (
     <div>
-      <p className="header">Featured Artworks</p>
-      <div className="carousel">
-        <div className="arrow">
-          <img
-            src={icons.leftarrow}
-            className="arrow-img left"
-            onClick={(event) => changeArtworkOnDisplay(openArtwork, -1)}
-          />
-        </div>
-        {artworks
-          .filter((artwork_) => artwork_.featured === true)
-          .map((artwork, ind) => (
-            <div
-              className={`artwork ${ind === openArtwork ? "open" : ""}`}
-              style={{
-                background: `url(${artwork.thumbnailURL})`,
-                backgroundSize: "cover",
-              }}
-              src={artwork.thumbnailURL}
-              onClick={(event) => changeArtworkOnDisplay(ind, 0)}
-              key={ind}
-            />
-          ))}
-        <div className="arrow">
-          <img
-            src={icons.rightarrow}
-            className="arrow-img right"
-            onClick={(event) => changeArtworkOnDisplay(openArtwork, 1)}
-          />
-        </div>
+      <div className={artworksLoading ? "spinner-container" : ""}>
+        <BarLoader loading={artworksLoading} />
       </div>
+      {!artworksLoading && (
+        <div>
+          <p className="header">Featured Artworks</p>
+          <div className="carousel">
+            <div className="arrow">
+              <img
+                src={icons.leftarrow}
+                className="arrow-img left"
+                onClick={(event) => changeArtworkOnDisplay(openArtwork, -1)}
+              />
+            </div>
+            {artworks
+              .filter((artwork_) => artwork_.featured === true)
+              .map((artwork, ind) => (
+                <div
+                  className={`artwork ${ind === openArtwork ? "open" : ""}`}
+                  style={{
+                    background: `url(${artwork.thumbnailURL})`,
+                    backgroundSize: "cover",
+                  }}
+                  src={artwork.thumbnailURL}
+                  onClick={(event) => changeArtworkOnDisplay(ind, 0)}
+                  key={ind}
+                />
+              ))}
+            <div className="arrow">
+              <img
+                src={icons.rightarrow}
+                className="arrow-img right"
+                onClick={(event) => changeArtworkOnDisplay(openArtwork, 1)}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
