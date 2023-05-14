8535a412c5f7955d40001f46ac0289097d25effe
iarshtejay <arshtejay@gmail.com>
2023-05-14 12:27:49 -0300

    feat: back button

--- src/components/CategoryGrid/CategoryGrid.js
+++ src/components/CategoryGrid/CategoryGrid.js
@@ -1,14 +1,33 @@
 import React from "react";
 import "./CategoryGrid.css";
 import categories from "../../data/categories.json";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import icons from "../../icons/icons";
 
 const CategoryGrid = ({ isHome }) => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
       {isHome && <p className="CategoryGrid-header">Categories</p>}
       <div className="CategoryGrid-container">
+        {!isHome && (
+          <div className="CategoryGrid-link" onClick={goBack}>
+            <div className={`CategoryGrid-item${isHome ? "" : "-condensed"}`}>
+              <img
+                src={icons["leftarrow"]}
+                className={`CategoryGrid-icon${isHome ? "" : "-condensed"}`}
+              />
+              <p>
+                <div className="CategoryGrid-link-highlight" />
+                Back
+              </p>
+            </div>
+          </div>
+        )}
         {categories.map((category) => {
           return (
             <NavLink
@@ -26,7 +45,7 @@ const CategoryGrid = ({ isHome }) => {
                   className={`CategoryGrid-icon${isHome ? "" : "-condensed"}`}
                 />
                 <p>
-                <div className="CategoryGrid-link-highlight" />
+                  <div className="CategoryGrid-link-highlight" />
                   {category}
                 </p>
               </div>
