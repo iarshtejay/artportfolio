105f72962ceae2b3892ea14d45b935e63cd42186
iarshtejay <arshtejay@gmail.com>
2023-07-05 20:59:30 -0300

    feat: improve accessibility for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,15 +5,28 @@ import icons from "../../icons/icons";
 
 const Footer = () => {
   const socials = [
-    { icon: icons.linkedin, href: "https://www.linkedin.com/in/arshdeep-singh09/" },
-    { icon: icons.github, href: "https://github.com/iarshtejay/artportfolio" },
-    { icon: icons.mail, href: "mailto:arshtejay@gmail.com" },
+    {
+      icon: icons.linkedin,
+      href: "https://www.linkedin.com/in/arshdeep-singh09/",
+      name: "LinkedIn",
+    },
+    {
+      icon: icons.github,
+      href: "https://github.com/iarshtejay/artportfolio",
+      name: "Source Code",
+    },
+    { icon: icons.mail, href: "mailto:arshtejay@gmail.com", name: "e-mail" },
   ];
   const renderSocials = () => {
     return socials.map((social) => (
       <div>
-        <a href={social.href}>
-          <img className="Footer-icon" src={social.icon} />
+        <a
+          href={social.href}
+          role="Button"
+          aria-label={`Link to ${social.name}`}
+          tabIndex={0}
+        >
+          <img className="Footer-icon" src={social.icon} alt="" />
         </a>
         <span>{"   "}</span>
       </div>
